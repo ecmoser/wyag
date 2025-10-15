74c8c912490418da9e1181ebc422056768e7d13b
ecmoser <78171469+ecmoser@users.noreply.github.com>
2025-10-14 23:17:37 -0400

    baseline with repo class and helper functions

---
+++ libwyag.py
@@ -0,0 +1,143 @@
+import argparse
+import configparser
+from datetime import datetime
+import grp, pwd
+from fnmatch import fnmatch
+import hashlib
+from math import ceil
+import os
+import re
+import sys
+import zlib
+
+# Create parser objects to parse command line arguments
+argparser = argparse.ArgumentParser(description="The stupidest content tracker")
+argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
+argsubparsers.required = True
+
+def main(argv=sys.argv[1:]):
+    # Parse command line arguments
+    args = argparser.parse_args(argv)
+
+    # Call bridge functions based on args
+    match args.command:
+        case "add"          : cmd_add(args)
+        case "cat-file"     : cmd_cat_file(args)
+        case "check-ignore" : cmd_check_ignore(args)
+        case "checkout"     : cmd_checkout(args)
+        case "commit"       : cmd_commit(args)
+        case "hash-object"  : cmd_hash_object(args)
+        case "init"         : cmd_init(args)
+        case "log"          : cmd_log(args)
+        case "ls-files"     : cmd_ls_files(args)
+        case "ls-tree"      : cmd_ls_tree(args)
+        case "rev-parse"    : cmd_rev_parse(args)
+        case "rm"           : cmd_rm(args)
+        case "show-ref"     : cmd_show_ref(args)
+        case "status"       : cmd_status(args)
+        case "tag"          : cmd_tag(args)
+        case _              : print("Bad command.")
+    
+class GitRepository(object):
+    """A git repository"""
+
+    worktree = None
+    gitdir = None
+    conf = None
+
+    def __init__(self, path, force=False):
+        # Initialize file path locations
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
+
+        # Check if path exists (ignore if forced)
+        if not (force or os.path.isdir(self.gitdir)):
+            raise Exception(f"Not a Git repository {path}")
+
+        # Read config file in .git/config
+        self.conf = configparser.ConfigParser()
+        cf = repo_file(self, config)
+
+        # Check if config file exists
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+        elif not force:
+            raise Exception("Configuration file missing.")
+
+        # Check to make sure repo is supported format
+        if not force:
+            vers = int(self.conf.get("core", "repositoryformatversion"))
+            if vers != 0:
+                raise Exception(f"Unsupported repositoryformatversion: {vers}")
+
+
+def repo_path(repo, *path):
+    """Compute path under repo's getdir."""
+    return os.path.join(repo.getdir, *path)
+
+
+def repo_file(repo, *path, mkdir=False):
+    """Same as repo_path, but create dirname(*path) if absent.  For
+example, repo_file(r, \"refs\", \"remotes\", \"origin\", \"HEAD\") will create
+.git/refs/remotes/origin."""
+
+    # If path is valid and a dir, return the path
+    if repo_dir(repo, path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
+
+    
+def repo_dir(repo, *path, mkdir=False):
+    """Same as repo_path, but mkdir *path if absent if mkdir"""
+
+    path = repo_path(repo, *path)
+
+    # If the path exists, make sure its a directory
+    if os.path.exists(path):
+        if os.path.isdir(path):
+            return path
+        else:
+            return Exception(f"Not a directory {path}")
+    
+    # If making new directories, do it and return the path
+    if mkdir:
+        os.makedirs(path)
+        return path
+    else:
+        return None
+    
+
+def repo_create(path):
+    """Create a new repository at path"""
+
+    repo = GitRepository(path, True)
+
+    # First, make sure the path either doesn't exist or is an empty directory.
+    if os.path.exists(repo.worktree):
+        if not os.path.isdir(repo.worktree):
+            raise Exception(f"{path} is not a directory")
+        if os.path.exists(repo.getdir) and os.listdir(repo.getdir):
+            raise Exception(f"{path} is not empty")
+    else:
+        os.makedirs(repo.worktree)
+
+    # Make sure all sub directories exist
+    assert repo_dir(repo, "branches", mkdir=True)
+    assert repo_dir(repo, "objects", mkdir=True)
+    assert repo_dir(repo, "refs", "tags", mkdir=True)
+    assert repo_dir(repo, "refs", "heads", mkdir=True)
+
+    # write to .git/description
+    with open(repo_file(repo, "description"), "w") as f:
+        f.write("Unnamed repository; edit this file 'description' to name the repository.\n")
+
+    # write to .git/HEAD
+    with open(repo_file(repo, "HEAD"), "w") as f:
+        f.write("ref: refs/heads/master\n")
+
+    # write from .git/config to config
+    with open(repo_file(repo, "config"), "w") as f:
+        config = repo_default_config()
+        config.write(f)
+
+    return repo
+    
